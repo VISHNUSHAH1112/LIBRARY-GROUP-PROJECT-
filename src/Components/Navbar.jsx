2e9429fac57889f8a0e889b18a64c57170443bec
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-08-22 11:54:57 +0530

    DHRUVI
    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -65,9 +65,7 @@ function LibraryNavbar() {
             {/* Dropdown */}
             {isLoggedIn && role === "admin" && (
               <Dropdown>
-                <Dropdown.Toggle variant="outline-info" id="dropdown-basic">
-                  Records
-                </Dropdown.Toggle>
+              
 
                 <Dropdown.Menu>
                   <Dropdown.Item as={NavLink} to="/Issues">
