938e57eee1248496e73759b4d5b6fff8beaa3493
VIshnu Shah <shahvishnu1112@gmail.com>
2025-09-03 20:48:01 +0530

    FinesSliceCreate

---
+++ src/Slice/FineSlice.js
@@ -0,0 +1,67 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const FetchFines = createAsyncThunk("FetchFines", async () => {
+  const res = await axios.get("http://localhost:3000/fines");
+  return res.data;
+});
+
+export const AddFines = createAsyncThunk("AddFines", async (newFines) => {
+  const res = await axios.post("http://localhost:3000/fines", newFines);
+  return res.data;
+});
+
+export const DeleteFines = createAsyncThunk("DeleteFines", async (id) => {
+  const res = await axios.delete(`http://localhost:3000/fines/${id}`);
+  return id;
+});
+
+const FinesSlice = createSlice({
+  name: "Fines",
+  initialState: {
+    fines: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(FetchFines.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(FetchFines.fulfilled, (state, action) => {
+        state.loading = false;
+        state.fines = action.payload;
+      })
+      .addCase(FetchFines.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+
+      .addCase(AddFines.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(AddFines.fulfilled, (state, action) => {
+        state.loading=false
+        state.fines.push(action.payload);
+      })
+      .addCase(AddFines.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(DeleteFines.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(DeleteFines.fulfilled, (state, action) => {
+        state.loading=false
+        state.fines = state.fines.filter(
+          (fines) => fines.id !== action.payload
+        );
+      })
+      .addCase(DeleteFines.rejected, (state, action) => {
+        state.loading=false
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default FinesSlice.reducer;
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,11 +1,13 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
-import booksReducer from "../Slice/BooksSlice";   // slice ko import karna hai
+import booksReducer from "../Slice/BooksSlice"; // slice ko import karna hai
+import Fines from "../Slice/FineSlice";
 
 // Redux store create karna
 export const store = configureStore({
   reducer: {
-    books: booksReducer,   // yaha slice add kiya
+    books: booksReducer, // yaha slice add kiya
+    fines: Fines,
   },
 });
 
